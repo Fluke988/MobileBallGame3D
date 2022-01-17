4c2bcef5b5310f90761bd66bb154ba5cd092a0c2
Fluke988 <jagtaphrushi@gmail.com>
2022-01-17 19:53:17 +0530

    Created a project

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    [SerializeField]
+    float ballSpeed;
+    Rigidbody rb;
+    bool gameStarted;
+    //bool gameOver;
+    float xVal, zVal;
+    
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        gameStarted = false;
+        //gameOver = false;
+    }
+    
+    void Start()
+    {
+        xVal = Random.Range(-4.5f, 4.5f);
+        zVal = Random.Range(-9f, -1f);
+        transform.position = new Vector3(xVal, 0, zVal);
+        //rb.velocity = new Vector3(ballspeed,0,0);
+    }
+
+    void Update()
+    {
+        if (!gameStarted)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                rb.velocity = new Vector3(ballSpeed, 0, 0);
+                gameStarted = true;
+                
+            }
+        }
+
+        //Debug.DrawRay(transform.position, Vector3.down, Color.black);
+
+        //if(!Physics.Raycast(transform.position, Vector3.down, 1f))
+        //{
+        //    rb.velocity = new Vector3(0, -25f, 0);
+        //    //gameOver = true;
+            
+        //}
+        
+        if (Input.GetMouseButtonDown(0) /*&& !gameOver*/)
+        {
+            ChangeBallDirection();
+        }
+
+        //if (gameOver == true)
+        //{
+        //    Destroy(gameObject, 3f);
+        //}
+    }
+
+    void ChangeBallDirection()
+    {
+        if (rb.velocity.z > 0)
+        {
+            rb.velocity = new Vector3(ballSpeed, 0, 0);
+        }
+        else if (rb.velocity.x > 0)
+        {
+            rb.velocity = new Vector3(0, 0, ballSpeed);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.tag.Equals("ground"))
+        {
+            Debug.Log("Game Over!!!");
+            Destroy(gameObject, 2f);
+        }
+    }
+}
---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform targetObject;
+    private Vector3 initalOffset;
+    private Vector3 cameraPosition;
+
+    void Start()
+    {
+        initalOffset = transform.position - targetObject.position;
+    }
+
+    void FixedUpdate()
+    {
+        cameraPosition = targetObject.position + initalOffset;
+        transform.position = cameraPosition;
+    }
+}
